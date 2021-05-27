4b20e70cbccd614d12f3f6311f20329921806688
Bryce <brycethorson.design@gmail.com>
2021-05-26 20:13:54 -0400

    added hero component. added scss dependencies.

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        thorson-design-co
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="hero-container">
+      <h1 class="title">{{ heroMain }} </h1>
+      <p class="subtitle"> {{heroSubText}} </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      title: 'Thorson Design Co.',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'UI, UX, Development'
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      heroMain: 'Thorson Design Co.',
+      heroSubText: `Hi, my name is Bryce Thorson, and I'm a software designer and developer living and working in Atlanta.
+	I help agencies, startups & businesses large and small bring their ideas to life through a refined design & development process.`
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -39,9 +39,12 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
+  & .hero-container {
+    display: flex;
+    flex-direction: column;
+    max-width: calc(100vw - 6rem);
+  }
+  & .title {
   font-family:
     'Quicksand',
     'Source Sans Pro',
@@ -57,15 +60,17 @@ export default {}
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
+  }
+  & .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+
 
 .links {
   padding-top: 15px;
